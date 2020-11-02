381d7948eb8bd1fe7e4275723045a720ea20e862
Merill Fernando <merill@merill.net>
2020-11-02 15:45:07 +1100

    Added unit tests

--- src/InclusivenessAnalyzer/InclusivenessAnalyzer.Test/InclusivenessAnalyzerUnitTests.cs
+++ src/InclusivenessAnalyzer/InclusivenessAnalyzer.Test/InclusivenessAnalyzerUnitTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 using VerifyCS = InclusivenessAnalyzer.Test.CSharpCodeFixVerifier<
     InclusivenessAnalyzer.InclusivenessAnalyzer,
@@ -20,7 +21,7 @@ namespace InclusivenessAnalyzer.Test
 
         //Diagnostic and CodeFix both triggered and checked for
         [TestMethod]
-        public async Task WhitelistTest()
+        public async Task WhitelistTypeNameTest()
         {
             var test = @"
     using System;
@@ -32,12 +33,19 @@ namespace InclusivenessAnalyzer.Test
 
     namespace ConsoleApplication1
     {
-        class {|#0:WhiteList|}
+        class WhiteList
         {   
         }
-    }";
+    }";  
 
-            var fixtest = @"
+            var expected = VerifyCS.Diagnostic("InclusivenessAnalyzer").WithLocation(11,15).WithArguments("WhiteList", "AllowList");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task WhitelistMethodNameTest()
+        {
+            var test = @"
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -47,12 +55,84 @@ namespace InclusivenessAnalyzer.Test
 
     namespace ConsoleApplication1
     {
-        class AllowList
+        class Program
         {   
+            public void WhiteList() { }
         }
     }";
 
-            var expected = VerifyCS.Diagnostic("InclusivenessAnalyzer").WithLocation(0).WithArguments("WhiteList");
+            var expected = VerifyCS.Diagnostic("InclusivenessAnalyzer").WithLocation(13, 25).WithArguments("WhiteList", "AllowList");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task WhitelistParameterTest()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            public void DoWork(string whiteList) { }
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic("InclusivenessAnalyzer").WithLocation(13, 39).WithArguments("whiteList", "AllowList");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task BlacklistPropertyTest()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            public int BlacklistNumber { get; set; }
+        }
+    }";
+
+            var expected1 = VerifyCS.Diagnostic("InclusivenessAnalyzer").WithLocation(13, 24).WithArguments("BlacklistNumber", "DenyList");
+            var expected2 = VerifyCS.Diagnostic("InclusivenessAnalyzer").WithLocation(13, 42).WithArguments("get_BlacklistNumber", "DenyList");
+            var expected3 = VerifyCS.Diagnostic("InclusivenessAnalyzer").WithLocation(13, 47).WithArguments("set_BlacklistNumber", "DenyList");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected1, expected2, expected3);
+        }
+
+        [TestMethod]
+        public async Task BlacklistFieldTest()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            public string BlackListValue;
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic("InclusivenessAnalyzer").WithLocation(13, 27).WithArguments("BlackListValue", "DenyList");
             await VerifyCS.VerifyAnalyzerAsync(test, expected);
         }
     }
